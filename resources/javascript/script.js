0a616aba77bf433fffcd5fe88d45c85089c52e1f
Abmiram <ross.birdsall9@gmail.com>
2020-09-26 16:31:49 -0400

    initial commit

---
+++ resources/javascript/script.js
@@ -0,0 +1,23 @@
+const password = 'PizzaPie';
+
+let element = document.querySelector("button");
+let violinLinks = document.getElementById("violin-box");
+let guitarLinks = document.getElementById("guitar-box");
+let percussionLinks = document.getElementById("percussion-box");
+let compLinks = document.getElementById("comp-box");
+let passwordBox = document.getElementById("pass-form");
+
+const showLinks = () => {
+    let pwdInput = document.getElementById("pword").value;
+    if (pwdInput === password) {
+        compLinks.hidden = false;
+        percussionLinks.hidden = false;
+        guitarLinks.hidden = false;
+        violinLinks.hidden = false;
+        passwordBox.hidden = true;
+    } else {
+        alert('Incorrect password entered.');
+    }
+};
+
+element.onclick = showLinks;
